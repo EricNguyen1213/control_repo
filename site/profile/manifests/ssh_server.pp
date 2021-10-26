b1423222a18566f8f112f42fe6ce98d0feedcf1d
Eric <eric.nguyenblank@gmail.com>
2021-10-26 12:56:20 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6eXpa4Wyjw3IrnBy+pm8bWWJbP01uRQaWOhn9Pq29DbJhpcoLevks2WotOeop1ZZICx/K2H7pWh3dkNy0XmEIaajIYAzj1iSJUs/RBcrI4vdHnDk49lU+03ue86CosJQFoNEUXic/owX3zafMGHJFqSQmwS6kbYq5HAL60A26IFTwmOfcsW7HcPCprDmL/rNyo/mWAdgoJlB80I48JPhIUP7ATZ2JL8H8EmUe1UKVCHkx6qWmCbcWil/OVXXVSHLRBy8k7KYRcaU3yesWNzpiE1yHGEgvRtsPrVu+thftjJRtLk0Y9N6EUfQbl9YJI7yCGqEmf3KKljDCA9JykM/H',
 	}  
 }
